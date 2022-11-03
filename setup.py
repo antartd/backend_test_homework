ff324c40f8b8ce10ee081767339e57cb176c9957
Dmitry Ant <dmimev@gmail.com>
2022-11-03 15:54:55 +0300

    final commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+	name='backend_test_homework',
+	version='1',
+	author='Dmitry Ant',
+	author_email='dmimev@gmail.com',
+	description='Ya.Praktik homework',
+	install_requires=[
+		'pytest==>0',
+	],
+)
